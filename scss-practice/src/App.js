d7da26bb9086e10a5aca9b643e99607885c2da66
yyyn012 <yyyn12_@naver.com>
2024-09-28 18:50:48 +0900

    Suspense & lazy

--- scss-practice/src/App.js
+++ scss-practice/src/App.js
@@ -1,48 +1,41 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Today from "./pages/Today";
-import Developer from "./pages/Developer";
-import Webd from "./pages/Webd";
-import Website from "./pages/Website";
-import Gsap from "./pages/Gsap";
-import Port from "./pages/Port";
-import Youtube from "./pages/Youtube";
-import Channel from "./pages/Channel";
-import Video from "./pages/Video";
-import Search from "./pages/Search";
-import Not from "./pages/Not";
-
-import Header from "./components/section/Header";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/section/Main";
-import Footer from "./components/section/Footer";
 
-function App() {
+const Home = lazy(() => import("./pages/Home"));
+const Today = lazy(() => import("./pages/Today"));
+const Developer = lazy(() => import("./pages/Developer"));
+const Webd = lazy(() => import("./pages/Webd"));
+const Website = lazy(() => import("./pages/Website"));
+const Gsap = lazy(() => import("./pages/Gsap"));
+const Port = lazy(() => import("./pages/Port"));
+const Youtube = lazy(() => import("./pages/Youtube"));
+const Channel = lazy(() => import("./pages/Channel"));
+const Video = lazy(() => import("./pages/Video"));
+const Search = lazy(() => import("./pages/Search"));
+const Not = lazy(() => import("./pages/Not"));
+
+const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/today" element={<Today />} />
-            <Route path="/developer" element={<Developer />} />
-            <Route path="/webd" element={<Webd />} />
-            <Route path="/website" element={<Website />} />
-            <Route path="/gsap" element={<Gsap />} />
-            <Route path="/port" element={<Port />} />
-            <Route path="/youtube" element={<Youtube />} />
-            <Route path="/channel/:channelID" element={<Channel />} />
-            <Route path="/video/:videoID" element={<Video />} />
-            <Route path="/search/:searchID" element={<Search />} />
-            <Route path="/*" element={<Not />} />
-          </Routes>
-        </Main>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<Main />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gsap />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelId" element={<Channel />} />
+          <Route path="/video/:videoId" element={<Video />} />
+          <Route path="/search/:searchId" element={<Search />} />
+          <Route path="*" element={<Not />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- scss-practice/src/components/section/Main.jsx
+++ scss-practice/src/components/section/Main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-const Main = () => {
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Main = (props) => {
   return (
-    <main id="main" role="main">
-      Main
-    </main>
+    <div>
+      <Header />
+      <main id="main" role="main">
+        {props.children}
+      </main>
+      <Footer />
+    </div>
   );
 };
 
