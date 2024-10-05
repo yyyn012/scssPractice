f33313d28590be6d1e971782b8e73fab49c0ad4b
yyyn012 <yyyn12_@naver.com>
2024-10-05 21:09:14 +0900

    Update Home.jsx

--- scss-practice/src/pages/Home.jsx
+++ scss-practice/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Main from "../components/section/Main";
+import Today from "../components/contents/Today";
+import Developer from "../components/contents/Developer";
+import Webd from "../components/contents/Webd";
+import Website from "../components/contents/Website";
+import Gsap from "../components/contents/Gsap";
+import Portfolio from "../components/contents/Portfolio";
+import Youtube from "../components/contents/Youtube";
 
 const Home = () => {
   return (
     <Main title="scss-practice youtube" description="유튜브 페이지입니다.">
-      Home
+      <Today />
+      <Developer />
+      <Webd />
+      <Website />
+      <Gsap />
+      <Portfolio />
+      <Youtube />
     </Main>
   );
 };
