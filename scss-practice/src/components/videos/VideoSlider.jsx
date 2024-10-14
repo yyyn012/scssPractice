4b4f254d2d83e584d5f5cba2d9999e07a705b295
yyyn012 <yyyn12_@naver.com>
2024-10-14 22:31:36 +0900

    Swiper $ SwiperSlide

--- scss-practice/src/components/videos/VideoSlider.jsx
+++ scss-practice/src/components/videos/VideoSlider.jsx
@@ -1,20 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+
 const VideoSlider = ({ videos, title, id }) => {
   return (
     <section id={id}>
       <h2>{title}</h2>
-      <div className="video__inner">
-        {videos.map((video, key) => (
-          <div className="video" key={key}>
-            <div className="video__thumb play__icon">
-              <Link to={`/video/${video.videoId}`}>
-                <img src={video.img} alt={video.title} />
-              </Link>
-            </div>
-          </div>
-        ))}
+      <div className="video__slider">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={20}
+          navigation={true}
+          modules={[Navigation]}
+          className={`mySwiper-${id}`}
+          breakpoints={{
+            640: { slidesPerView: 2, spaceBetween: 20 },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            1600: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          {videos.map((video, key) => (
+            <SwiperSlide key={key}>
+              <div className="video" key={key}>
+                <div className="video__thumb play__icon">
+                  <Link to={`/video/${video.videoId}`}>
+                    <img src={video.img} alt={video.title} />
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
