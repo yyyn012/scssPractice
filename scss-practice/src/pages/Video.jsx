bb1d86f778a3fdf6350c09853d3b7ba923b4d62b
yyyn012 <yyyn12_@naver.com>
2024-10-23 20:35:35 +0900

    Get videoID value & Update video.scss

--- scss-practice/src/pages/Video.jsx
+++ scss-practice/src/pages/Video.jsx
@@ -1,13 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/api";
+
 import Main from "../components/section/Main";
+import ReactPlayer from "react-player";
+
+import { CiChat1 } from "react-icons/ci";
+import { CiStar } from "react-icons/ci";
+import { CiRead } from "react-icons/ci";
 
 const Video = () => {
+  const { videoId } = useParams();
+  const [videoDetail, setVideoDetail] = useState(null);
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${videoId}`).then(
+      (data) => {
+        console.log(data);
+        setVideoDetail(data.items[0]);
+      }
+    );
+  }, [videoId]);
+
   return (
     <Main
       title="유튜브 비디오 영상"
       description="유튜브 비디오 영상을 볼 수 있습니다."
     >
-      Video
+      <section id="videoViewPage">
+        {videoDetail && (
+          <div className="video__view">
+            <div className="video__play">
+              <ReactPlayer
+                playing={true}
+                url={`https://www.youtube.com/watch?v=${videoId}`}
+                width="100%"
+                heigth="100%"
+                style={{ position: "absolute", top: 0, left: 0 }}
+              />
+              <div className="video__info">
+                <h2 className="video__title">{videoDetail.snippet.title}</h2>
+                <div className="video__channel">
+                  <div className="id">
+                    <Link to="/channel">
+                      {videoDetail.snippet.channelTitle}
+                    </Link>
+                  </div>
+                  <div className="count">
+                    <span className="view">
+                      <CiRead />
+                      {videoDetail.statistics.viewCount}
+                    </span>
+                    <span className="like">
+                      <CiStar />
+                      {videoDetail.statistics.likeCount}
+                    </span>
+                    <span className="comment">
+                      <CiChat1 />
+                      {videoDetail.statistics.commentCount}
+                    </span>
+                  </div>
+                </div>
+                <div className="video__desc">
+                  {videoDetail.snippet.description}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
     </Main>
   );
 };
