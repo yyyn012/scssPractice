a011b62e078e577e28db34076ec7221c67a374c5
yyyn012 <yyyn12_@naver.com>
2024-10-24 16:10:58 +0900

    Update Channel.jsx

--- scss-practice/src/pages/Channel.jsx
+++ scss-practice/src/pages/Channel.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/api";
+
 import Main from "../components/section/Main";
 
+import { CiBadgeDollar } from "react-icons/ci";
+import { CiMedal } from "react-icons/ci";
+import { CiRead } from "react-icons/ci";
+
 const Channel = () => {
+  const { channelId } = useParams();
+  const [channelDetail, setChannelDetail] = useState();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${channelId}`);
+      setChannelDetail(data.items[0]);
+    };
+    fetchResults();
+  }, [channelId]);
+
   return (
     <Main title="유튜브 채널" description="유튜브 채널페이지입니다.">
-      Channel
+      {channelDetail && (
+        <setcion id="channel">
+          <div
+            className="channel__header"
+            style={{
+              backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})`,
+            }}
+          >
+            <div className="circle">
+              <img
+                src={channelDetail.snippet.thumbnails.high.url}
+                alt={channelDetail.snippet.title}
+              />
+            </div>
+          </div>
+          <div className="channel__info">
+            <h3 className="title">{channelDetail.snippet.title}</h3>
+            <p className="desc">{channelDetail.snippet.description}</p>
+            <div className="info">
+              <span>
+                <CiBadgeDollar />
+                {channelDetail.statistics.subscriberCount}
+              </span>
+              <span>
+                <CiMedal />
+                {channelDetail.statistics.videoCount}
+              </span>
+              <span>
+                <CiRead />
+                {channelDetail.statistics.viewCount}
+              </span>
+            </div>
+          </div>
+        </setcion>
+      )}
     </Main>
   );
 };
