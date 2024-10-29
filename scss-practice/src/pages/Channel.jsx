f99318b49f8702b24530420b46ed64abf8d03703
yyyn012 <yyyn12_@naver.com>
2024-10-29 19:22:37 +0900

    Update Channel.jsx (Add channel video)

--- scss-practice/src/pages/Channel.jsx
+++ scss-practice/src/pages/Channel.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { fetchFromAPI } from "../utils/api";
 
 import Main from "../components/section/Main";
+import VideoSearch from "../components/videos/VideoSearch";
 
 import { CiBadgeDollar } from "react-icons/ci";
 import { CiMedal } from "react-icons/ci";
@@ -11,6 +12,7 @@ import { CiRead } from "react-icons/ci";
 const Channel = () => {
   const { channelId } = useParams();
   const [channelDetail, setChannelDetail] = useState();
+  const [channelVideo, setChannelVideo] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -20,6 +22,11 @@ const Channel = () => {
           `channels?part=snippet&id=${channelId}`
         );
         setChannelDetail(data.items[0]);
+
+        const videosData = await fetchFromAPI(
+          `search?channelId=${channelId}&part=snippet%2Cid&order=date`
+        );
+        setChannelVideo(videosData?.items);
       } catch (error) {
         console.error("Error fetching data : ", error);
       } finally {
@@ -33,42 +40,46 @@ const Channel = () => {
 
   return (
     <Main title="유튜브 채널" description="유튜브 채널페이지입니다.">
-      <section id="channel" className={channelPageClass}></section>
-      {channelDetail && (
-        <div className="channel__inner">
-          <div
-            className="channel__header"
-            style={{
-              backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})`,
-            }}
-          >
-            <div className="circle">
-              <img
-                src={channelDetail.snippet.thumbnails.high.url}
-                alt={channelDetail.snippet.title}
-              />
+      <section id="channel" className={channelPageClass}>
+        {channelDetail && (
+          <div className="channel__inner">
+            <div
+              className="channel__header"
+              style={{
+                backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})`,
+              }}
+            >
+              <div className="circle">
+                <img
+                  src={channelDetail.snippet.thumbnails.high.url}
+                  alt={channelDetail.snippet.title}
+                />
+              </div>
+            </div>
+            <div className="channel__info">
+              <h3 className="title">{channelDetail.snippet.title}</h3>
+              <p className="desc">{channelDetail.snippet.description}</p>
+              <div className="info">
+                <span>
+                  <CiBadgeDollar />
+                  {channelDetail.statistics.subscriberCount}
+                </span>
+                <span>
+                  <CiMedal />
+                  {channelDetail.statistics.videoCount}
+                </span>
+                <span>
+                  <CiRead />
+                  {channelDetail.statistics.viewCount}
+                </span>
+              </div>
+            </div>
+            <div className="channel__video video__inner search">
+              <VideoSearch videos={channelVideo} />
             </div>
           </div>
-          <div className="channel__info">
-            <h3 className="title">{channelDetail.snippet.title}</h3>
-            <p className="desc">{channelDetail.snippet.description}</p>
-            <div className="info">
-              <span>
-                <CiBadgeDollar />
-                {channelDetail.statistics.subscriberCount}
-              </span>
-              <span>
-                <CiMedal />
-                {channelDetail.statistics.videoCount}
-              </span>
-              <span>
-                <CiRead />
-                {channelDetail.statistics.viewCount}
-              </span>
-            </div>
-          </div>
-        </div>
-      )}
+        )}
+      </section>
     </Main>
   );
 };
