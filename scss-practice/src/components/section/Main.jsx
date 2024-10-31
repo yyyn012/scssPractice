08103e5edc934637f3553d4783383709da4c8f96
yyyn012 <yyyn12_@naver.com>
2024-10-31 13:52:59 +0900

    Create scrollTo.js

--- scss-practice/src/components/section/Main.jsx
+++ scss-practice/src/components/section/Main.jsx
@@ -4,10 +4,12 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Header from "./Header";
 import Footer from "./Footer";
 import Search from "./Search";
+import ScrollTo from "../../utils/scrollTo";
 
 const Main = (props) => {
   return (
     <HelmetProvider>
+      <ScrollTo />
       <Helmet
         titleTemplate="%s | Webs Youtube"
         defaultTitle="Webs Youtube"
